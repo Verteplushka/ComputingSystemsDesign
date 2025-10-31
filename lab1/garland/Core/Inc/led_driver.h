9638d57e12f4322832936283d3f6297d1d111a38
Verteplushka <kolbacin_vlad@mail.ru>
2025-10-31 17:02:30 +0300

    Added lab1

---
+++ lab1/garland/Core/Inc/led_driver.h
@@ -0,0 +1,16 @@
+#ifndef LED_DRIVER_H
+#define LED_DRIVER_H
+
+#include <stdint.h>
+
+#define LED_GREEN_PIN    GPIO_PIN_13
+#define LED_RED_PIN      GPIO_PIN_14
+#define LED_YELLOW_PIN   GPIO_PIN_15
+#define LED_PORT         GPIOD
+
+void LED_Init(void);
+void LED_SetMask(uint16_t mask); // бит 0 -> green, 1 -> red, 2 -> yellow
+void LED_ToggleMask(uint16_t mask);
+uint16_t LED_GetMask(void);
+
+#endif // LED_DRIVER_H
